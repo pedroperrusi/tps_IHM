0f3dc44587f28dd196de60cc88536e08ecee334d
--global <suruagyperrusi@etu.unistra.fr>
2018-12-13 17:57:42 +0100

    Supress button created

--- src/CI1_Editeur_Graphique2D/src/EditeurGraphique2D.java
+++ src/CI1_Editeur_Graphique2D/src/EditeurGraphique2D.java
@@ -74,6 +74,16 @@ public class EditeurGraphique2D extends JFrame
 		ajouter.addActionListener(actionAjout);
 		// ajout du bouton dans la fenetre
 		panel.add(ajouter);
+		
+		// création du bouton ajouter
+		final JButton supprimer = new JButton("-");
+		// création d’une action pour ajouter dans la liste
+		ActionSupress actionSupress = new ActionSupress();
+		// affectation de cette action au bouton
+		supprimer.addActionListener(actionSupress);
+		// ajout du bouton dans la fenetre
+		panel.add(supprimer);
+		
 		return panel;
 	}
 	
@@ -105,12 +115,6 @@ public class EditeurGraphique2D extends JFrame
 	
 	class ActionAjouter implements ActionListener
 	{
-
-		public ActionAjouter() 
-		{
-			super();
-		}
-		
 		@Override
 		public void actionPerformed(ActionEvent e)
 		{
@@ -126,24 +130,43 @@ public class EditeurGraphique2D extends JFrame
 			{
 				System.out.println(except.toString());
 			}
-		}
-		
-		Point2D randomPosition() 
+		}	
+	}
+	
+	class ActionSupress implements ActionListener
+	{	
+		@Override
+		public void actionPerformed(ActionEvent e)
 		{
-			float x = (float) (Math.random() * LARGEUR_PAR_DEFAUT);
-			float y = (float) (Math.random() * HAUTEUR_PAR_DEFAUT);
-			return new Point2D(x, y);
-		}
-		
-		int randomColor() 
-		{
-			return (int)(Math.random() * 255);
-		}
-		
-		float randomTaille() 
-		{
-			return (float) (50 + Math.random() * ( 150 - 50));
+			if(zoneDessin.listeFormes.size() == 0)
+				return;
+			try {
+				zoneDessin.listeFormes.remove(zoneDessin.listeFormes.size()-1);
+				zoneDessin.repaint();
+			}
+			catch(Exception except) 
+			{
+				System.out.println(except.toString());
+			}
 		}
 		
 	}
+	
+	Point2D randomPosition() 
+	{
+		float x = (float) (Math.random() * LARGEUR_PAR_DEFAUT);
+		float y = (float) (Math.random() * HAUTEUR_PAR_DEFAUT);
+		return new Point2D(x, y);
+	}
+	
+	int randomColor() 
+	{
+		return (int)(Math.random() * 255);
+	}
+	
+	float randomTaille() 
+	{
+		return (float) (50 + Math.random() * ( 150 - 50));
+	}
+	
 }
