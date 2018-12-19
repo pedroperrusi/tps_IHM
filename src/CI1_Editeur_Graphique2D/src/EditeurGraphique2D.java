f2f1eb3e2a275b17b1c88670fca9fa1091f0b0a0
--global <suruagyperrusi@etu.unistra.fr>
2018-12-19 14:40:26 +0100

    Keyboard inputs now creates forms

--- src/CI1_Editeur_Graphique2D/src/EditeurGraphique2D.java
+++ src/CI1_Editeur_Graphique2D/src/EditeurGraphique2D.java
@@ -3,6 +3,8 @@ package CI1_Editeur_Graphique2D.src;
 import java.awt.*;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
+import java.awt.event.KeyEvent;
+import java.awt.event.KeyListener;
 import java.util.ArrayList;
 import java.util.Collections;
 
@@ -119,6 +121,8 @@ public class EditeurGraphique2D extends JFrame
 		{
 			super();
 			listeFormes = new ArrayList<Forme2D>();
+			this.setFocusable(true);
+			this.addKeyListener(new KeyboardInputs());
 		}
 		
 		public void sortForme2D() 
@@ -223,4 +227,46 @@ public class EditeurGraphique2D extends JFrame
 		return (float) (MIN_FORM_TAILLE + Math.random() * (MAX_FORM_TAILLE - MIN_FORM_TAILLE));
 	}
 
+	class KeyboardInputs implements KeyListener
+	{
+
+		@Override
+		public void keyPressed(KeyEvent event)
+		{
+			char key = event.getKeyChar();
+			// Generate information for the form creation
+			Point2D origin = randomPosition();
+			Color color = new Color(randomColor(), randomColor(),
+					randomColor());
+			float taille = randomTaille();
+			// If user press r, draw square
+			if(key == 'r') 
+			{
+				zoneDessin.listeFormes.add(new Carre(origin, color, taille));
+				zoneDessin.repaint();
+			}
+			// If user press c, draw circle
+			if(key == 'c') 
+			{
+				zoneDessin.listeFormes.add(new Cercle(origin, color, taille));
+				zoneDessin.repaint();
+			}
+		}
+
+		@Override
+		public void keyReleased(KeyEvent event)
+		{
+			// TODO Auto-generated method stub
+			
+		}
+
+		@Override
+		public void keyTyped(KeyEvent event)
+		{
+			// TODO Auto-generated method stub
+			
+		}
+		
+	}
+	
 }
